fff503e773d1e5db4f230be83a5f803c8c4e8c24
Joe Losee <josephlosee@gmail.com>
2024-01-03 08:40:51 -0800

    Learn NextJS Chapter 3 - Optimizing Fonts & Images
    Follows chapter 3, adding multiple new fonts and two hero images.
    - Added Inter, Glitch, and Lusitana fonts.
    - Enabled the AcmeLogo component on page.tsx
    - Updated layout to use a main font.

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Rubik_Glitch, Lusitana } from 'next/font/google';
+
+export const inter = Inter({subsets: ['latin']});
+export const glitch = Rubik_Glitch({subsets: ['latin'], weight: "400"});
+export const lusitana = Lusitana({subsets: ['latin'], weight: "400"});
